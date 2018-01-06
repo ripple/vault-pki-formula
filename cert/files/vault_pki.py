51fe10e85e3146cba159543e310b94b7d0492822
Daniel Wilcox <dwilcox@ripple.com>
2018-01-06 07:39:20 +0000

    Update license pre-release for lawyer cats.

--- cert/files/vault_pki.py
+++ cert/files/vault_pki.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#   Copyright 2018 Ripple Labs, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
 """Command for managing a cert issued by way of the salt-master.
 
@@ -95,7 +109,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__author__ = 'Daniel Wilcox (dwilcox@ripple.com)'
+__author__ = 'Daniel Wilcox (dmw@ripple.com)'
 
 import argparse
 import datetime
